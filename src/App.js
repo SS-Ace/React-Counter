0afac06b14776e94da765de82ae2345a143b22f8
SS_Ace <SS_Ace@protonmail.com>
2021-09-04 14:52:22 +0530

    upload

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ function App() {
 
   const [value, setValue] = useState(0)
   return (
-    <div className="res-body">
+    <div>
+      <div className="res-body">
       <h1 className="header">Counter App</h1>
+      <div className="break"></div>
       <p className="display">{value}</p>
-      <button className="+" onClick={ () => setValue(value + 1)}>+</button>
-      <button className="-" onClick={ () => setValue(value - 1)}>-</button>
+      </div>
+      <div className="buttons">
+      <button className="inc" onClick={ () => setValue(value + 1)}>+</button>
+      <div className="break-col"></div>
+      <button className="dec" onClick={ () => setValue(value - 1)}>-</button>
+      <div className="break-col" ></div>
       <button className="reset" onClick={ () => setValue(0)}>Reset</button>
+    </div>
     </div>
   );
 }
